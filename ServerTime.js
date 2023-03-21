61c207fed1e4cde954e99ee675107c6476b4bc17
박정민 <jeongmin@bagjeongmin-ui-MacBookAir.local>
2023-03-21 20:17:29 +0900

    ServerTime.js Init

---
+++ ServerTime.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import {StyleSheet,View, Text } from 'react-native';
+
+export const ServerTime = (props) => {
+
+    
+    return(
+        <View style = {styles.container}>
+
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+
+})
